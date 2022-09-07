97ce707a6c9b5fc81a6ee86f6556af73058ed48d
changwoobin <88649829+changwoobin@users.noreply.github.com>
2022-09-07 22:23:09 +0900

    Add files via upload

--- map.py
+++ map.py
@@ -10,8 +10,8 @@ screen = pygame.display.set_mode((scrWidth, scrHeight))
 pygame.display.set_caption("Jump King")
 background = pygame.image.load("./img/background.png")
 block_side = pygame.image.load("./img/block.png") # 300 X 200
-block_center = pygame.image.load("./img/block2.png") # 360 X 100
-block_center2 = pygame.image.load("./img/block3.png")
+block_center = pygame.image.load("./img/block2.png") # 360 X 10
+block_center2 = pygame.image.load("./img/block3.png") # 360 X 100
 
 #캐릭터
 character = pygame.image.load("./img/end.png") # 10X10
@@ -23,12 +23,18 @@ y_pos = 540 - character_height - 10
 
 to_x = 0
 to_y = 0
+
 a_x = 0
-a_y = 0
 u = 0
+
+before_x = scrWidth/2 - 5
+before_y = 540 - character_height - 10
+
 
 time_jump = 0
 end_time_jump = 0
+
+
 running = True
 
 while running:
@@ -50,18 +56,18 @@ while running:
             if event.key == pygame.K_SPACE:
                 end_time_jump = time.time()/1.1
                 u = (end_time_jump - time_jump)*20*(-1)
-                if u < -25:
-                    u = -25
+                if u < -24:
+                    u = -24
                 
                 to_x = u*a_x
                 to_y = 0.9*u
+                a_x = 0
 
 
     x_pos += to_x
     y_pos += to_y
 
-    to_y += 1 
-
+    to_y += 1
 
     if y_pos > 520:
         y_pos = 520
@@ -73,15 +79,31 @@ while running:
     if x_pos > scrWidth - 10:
         x_pos = scrWidth - 10
 
+    if y_pos > 330:
+        if x_pos < 300:
+            if before_x > 300:
+                to_x *= -1
+            elif before_x < 300:
+                y_pos = 330
+                to_x = 0
+        elif x_pos > 650:
+            if before_x < 650:
+                to_x *= -1
+            elif before_x > 650:
+                y_pos = 330
+                to_x = 0
 
-    if y_pos < 330 and (x_pos < 300 or x_pos > 660):
-        y_pos = 330
+    if y_pos < 200 and before_y > 200 and x_pos > 300 and x_pos < 650:
+        to_y *= -1
+    elif y_pos > 100 and before_y < 100 and x_pos > 300 and x_pos < 650:
+        y_pos = 90
         to_x = 0
-    elif y_pos > 330 and x_pos > 650:
-        x_pos = 650
-    elif y_pos > 330 and x_pos < 300:
-        x_pos = 300
     
+    if y_pos < 200 and y_pos > 100 and  x_pos > 300 and before_x < 300:
+        to_x *= -1
+    elif y_pos < 200 and y_pos > 100 and  x_pos < 660 and before_x > 660:
+        to_x *= -1
+
 
     screen.blit(background, (0, 0))
     screen.blit(block_side, (0,340))
@@ -91,6 +113,8 @@ while running:
     screen.blit(character, (x_pos, y_pos))
 
     pygame.display.update()
+    before_x = x_pos
+    before_y = y_pos
     time.sleep(0.01)
 
 
